f840887c9c68cfa8987e45116d06abe96838b387
Jack Lee <jack77121@gmail.com>
2021-07-18 12:14:05 +0800

    add nominator info interface

--- src/apis/Nominator/index.tsx
+++ src/apis/Nominator/index.tsx
@@ -1,5 +1,23 @@
 import { nominatorAxios } from '../../instance/Axios';
 
+export interface INominatorInfo {
+  accountId: string;
+  balance: {
+    free_balance: number;
+    locked_balance: number;
+  };
+  targets: string[];
+  rewards: {
+    stash: string;
+    eraRewards: {
+      era: number;
+      amount: number;
+      timestamp: number;
+      price: number;
+      total: number;
+    }[];
+  };
+}
 export interface INominatorParams {
   chain: string;
   id: string;
@@ -7,7 +25,7 @@ export interface INominatorParams {
 export interface INominator {
   params: INominatorParams;
 }
-export const apiGetInfoNominator = (data: INominator) =>
+export const apiGetInfoNominator = (data: INominator): Promise<INominatorInfo[]> =>
   nominatorAxios.get(`${data.params.id}/${data.params.chain}`).then((res) => {
     return res.data;
   });
