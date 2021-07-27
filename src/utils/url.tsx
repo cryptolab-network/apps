04d4b760ffb450969d6d6dfa7a6d085b5cb28eb9
yaohsin <iisaintz@gmail.com>
2021-07-27 11:27:28 +0800

    make url to works with staing

--- src/utils/url.tsx
+++ src/utils/url.tsx
@@ -8,6 +8,12 @@ export const getUrls = (location: Location, subdomain: string): [string, string]
     domain = urlParts.slice(0).slice(-(urlParts.length === 4 ? 3 : 2)).join('.');
   }
 
+  if (location.hostname.indexOf('staging') !== -1) {
+    const staking_url = `${protocol}//staging.${domain}${(port !== '') ? ':' + port : port}`
+    const tools_url = `${protocol}//${subdomain}.staging.${domain}${(port !== '') ? ':' + port : port}`;
+    return [staking_url, tools_url];
+  }
+
   const staking_url = `${protocol}//${domain}${(port !== '') ? ':' + port : port}`
   const tools_url = `${protocol}//${subdomain}.${domain}${(port !== '') ? ':' + port : port}`;
   return [staking_url, tools_url];
