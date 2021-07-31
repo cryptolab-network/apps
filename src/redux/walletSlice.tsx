cdfad98aba6f8b4c5a115202ff6552bf78b57951
yaohsin <iisaintz@gmail.com>
2021-07-31 16:08:12 +0800

    fix polkadot prefix to 0

--- src/redux/walletSlice.tsx
+++ src/redux/walletSlice.tsx
@@ -7,7 +7,7 @@ import { hexToU8a, isHex } from '@polkadot/util';
 const networkInfo = [
   {
     name: 'Polkadot',
-    prefix: 1,
+    prefix: 0,
     genesisHash: '0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3',
   },
   {
