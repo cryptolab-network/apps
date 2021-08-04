9bd8191cdfa5bd9af6867133fb3fa107727c45f2
yktseng <tanistseng@gmail.com>
2021-08-04 15:50:13 +0800

    init commit for staking guide

---
+++ src/pages/Guide/components/GraphicalGuide.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const GraphicalGuide = () => {
+  return (
+    <GraphicalGuideLayout>
+    </GraphicalGuideLayout>
+  );
+};
+
+export default GraphicalGuide;
+
+const GraphicalGuideLayout = styled.div`
+  padding: 17.5px 17px 24.2px 16px;
+  border-radius: 10px;
+  background-color: #17212d;
+`;
--- src/pages/Guide/index.tsx
+++ src/pages/Guide/index.tsx
@@ -1,5 +1,32 @@
+import { Grid } from '@material-ui/core';
+import styled from 'styled-components';
+import GraphicalGuide from './components/GraphicalGuide';
+import TextGuide from './components/TextGuide';
+
 const Guide = () => {
-  return <div style={{ color: 'white' }}>Guide Page</div>;
+  return (
+    <GuideLayout>
+      <Grid container spacing={6}>
+        <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
+          <GraphicalGuide></GraphicalGuide>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={6} xl={6}>
+          <TextGuide />
+        </Grid>
+      </Grid>
+    </GuideLayout>
+  );
 };
 
 export default Guide;
+
+const GuideLayout = styled.div`
+  width: 80vw;
+  height: 80%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  margin: 150px 10vw 0 10vw;
+`;
+
