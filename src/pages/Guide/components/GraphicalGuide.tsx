105d04896d239a1d1f81ee96bf483d861a349583
yktseng <tanistseng@gmail.com>
2021-09-03 13:37:49 +0800

    add staking guide images

--- src/pages/Guide/components/GraphicalGuide.tsx
+++ src/pages/Guide/components/GraphicalGuide.tsx
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
+import { ReactComponent as WalletGuide } from '../../../assets/images/guide-wallet.svg';
+import { ReactComponent as StakingGuide } from '../../../assets/images/guide-staking.svg';
 
 const GraphicalGuide = () => {
   return (
     <GraphicalGuideLayout>
+      <WalletGuide />
+      <StakingGuide />
     </GraphicalGuideLayout>
   );
 };
@@ -13,4 +17,8 @@ const GraphicalGuideLayout = styled.div`
   padding: 17.5px 17px 24.2px 16px;
   border-radius: 10px;
   background-color: #17212d;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
