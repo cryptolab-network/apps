f4a636308a2efdcf38124bd2d722f3f9b67849aa
yaohsin <iisaintz@gmail.com>
2021-09-08 20:19:15 +0800

    debug

--- src/utils/ga.tsx
+++ src/utils/ga.tsx
@@ -2,6 +2,7 @@ import ReactGA from 'react-ga';
 import keys from '../config/keys';
 
 export const initGA = () => {
+  console.log(`ga: ${keys.gaTrackingId}`);
   ReactGA.initialize(keys.gaTrackingId);
 }
 
@@ -9,6 +10,7 @@ export const sendPageView = (location) => {
   const path = location.pathname + location.search;
   ReactGA.set({ page: path });
   ReactGA.pageview(path);
+  console.log(`sendPageView: ${path}`);
 }
 
 
